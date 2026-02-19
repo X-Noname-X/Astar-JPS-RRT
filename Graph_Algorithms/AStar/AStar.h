47bc22e05d20852c4e988784583424bbe0aea65b
X-Y-Q <xiao_yq317@163.com>
2026-02-20 00:10:31 +0800

    修改了Astar，代码更简洁，逻辑更合理

--- Graph_Algorithms/AStar/AStar.h
+++ Graph_Algorithms/AStar/AStar.h
@@ -3,39 +3,39 @@
 #define _ASTAR_H_
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <list>
 #include <algorithm>
 
 class Node
 {
 public:
 	std::pair<int, int> NodePos; //节点坐标{y,x}
-	int G, H, F; //G=起点到当前节点的实际代价，H当前节点到终点的代价（曼哈顿距离），F = G + H
+	float G, H, F; //G=起点到当前节点的实际代价，H当前节点到终点的代价（曼哈顿距离），F = G + H
 	Node* Parents; //父节点的指针
-	bool Block; //障碍物
-	Node() :NodePos(0, 0),G(0),H(0),F(0),Parents(NULL),Block(false){} //构造函数，成员赋值
-	void PlanCost(Node* currentnode, std::pair<int, int> goal_); //路径代价
+	Node() :NodePos(0, 0),G(0),H(0),F(0),Parents(nullptr){} //构造函数，成员赋值
+	void PathCost(Node* currentnode, std::pair<int, int> goal_, float G_cost); //路径代价
 };
 
 class AStar
 {
 public:
-	std::vector<std::vector<Node>> Map; //节点地图
+	std::vector<std::vector<int>> Map_Data; //存储地图的障碍物信息
 	std::pair<int, int> MapSize; //地图的尺寸 {y,x}
-	std::pair<int, int> StartNode; //起点
-	std::pair<int, int> GoalNode; //终点
-	std::vector<Node> OpenList;
-	std::vector<Node> CloseList;
+
+	std::list<Node> OpenList;
+	std::list<Node> CloseList;
+	std::vector<Node*> PathNode;
 
 	AStar(std::pair<int, int> mapsize); //构造函数，参数为地图尺寸
-	void SetBlock(std::vector<std::pair<int, int>> block);    //设置障碍物
+	std::pair<Node, std::list<Node>::iterator> getfLeastNode();
+	bool isInList(std::list<Node> List,Node searchNode); //判断探索节点是否已经在OpenList或者CloseList里
 	bool SearchPath(std::pair<int, int> start, std::pair<int, int> goal);  //路径搜索
-	void ShowMap();  //显示地图
 	void ShowPath(); //显示最短路径
 
 private:
-	//探索方向，每个节点仅探索上下左右4个方向   顺时针探索顺序{y,x}
-	int motions[4][2] = { {0,1},{-1,0},{0,-1},{1,0} };
+	//探索方向，每个节点支持探索8个方向   顺时针探索顺序{y,x}
+	float motions[8][3] = { {0,1,1},{-1,0,1},{0,-1,1},{1,0,1},
+						  {1,1,1.4},{-1,1,1.4},{-1,-1,1.4},{1,-1,1.4} };
 };
 
 #endif
---
+++ Graph_Algorithms/AStar/AStar_main.cpp
@@ -0,0 +1,10 @@
+#include "AStar.h"
+
+int main()
+{
+	AStar astar({1000,1000}); //operater{}  
+	bool path = astar.SearchPath({ 0,0 }, { 900,980 });
+	if (path) astar.ShowPath();
+	system("pause");
+	return 0;
+}
