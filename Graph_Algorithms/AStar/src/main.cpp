2861e105668aeff931baabf2515a1fb99edd8eed
X-Y-Q <xiao_yq317@163.com>
2026-02-21 22:50:44 +0800

    添加障碍物

--- Graph_Algorithms/AStar/src/main.cpp
+++ Graph_Algorithms/AStar/src/main.cpp
@@ -2,8 +2,13 @@
 
 int main()
 {
-	AStar astar({1000,1000}); //operater{}  
-	bool path = astar.SearchPath({ 0,0 }, { 900,980 });
+	// AStar astar({1000,1000}); //operater{}  
+	// bool path = astar.SearchPath({ 0,0 }, { 900,980 });
+	AStar astar({ 10,10 });
+	for (int i = 2; i <= 7; i++) astar.Map_Data[1][i] = 1;
+	for (int i = 2; i <= 7; i++) astar.Map_Data[8][i] = 1;
+	for (int i = 2; i <= 7; i++) astar.Map_Data[i][7] = 1;
+	bool path = astar.SearchPath({ 1,0 }, { 7,8 });
 	if (path) astar.ShowPath();
 	system("pause");
 	return 0;
